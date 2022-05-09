1a646e033de6984f128a5238166927388b578154
Sebastián Aguilera <sebastian@procodigo.cl>
2022-05-09 13:14:55 -0400

    Indicador descarga Giftcard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -42,6 +42,17 @@
                 </div>
             </div>
         </div>
+        <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box mb-3">
+                <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-file-download"></i></span>
+                <div class="info-box-content">
+                   <span class="info-box-text">Total descarga Giftcard</span>
+                    <span class="info-box-number">
+                        {{ option('countGiftcardDiaMadre',0) }}
+                    </span>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="row">
         <div class="col-md-3">
