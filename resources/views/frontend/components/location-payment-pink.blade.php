a2df9c6b5d399457588ad363eb681202e363f16f
Sebastián Aguilera <sebastian@procodigo.cl>
2023-09-21 13:11:54 -0300

    Corrección en imágenes landing

--- resources/views/frontend/components/location-payment-pink.blade.php
+++ resources/views/frontend/components/location-payment-pink.blade.php
@@ -3,7 +3,7 @@
         <img src="{{ asset('images/icon_loction_r.svg') }}" class="img-fluid">
     </div>
     <div class="col-md-4 px-0 px-sm-1 text-center mb-4">
-        <img src="{{ asset('images/icon_loction_r.svg') }}" class="img-fluid">
+        <img src="{{ asset('images/icon_loction_r.png') }}" class="img-fluid">
     </div>
     <div class="col-md-4 px-0 px-sm-1 text-center mb-4">
         <img src="{{ asset('images/Pay_drsmile.png') }}" class="img-fluid">
