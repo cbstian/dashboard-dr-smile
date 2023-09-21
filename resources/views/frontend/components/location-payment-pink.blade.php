50697029170c71e331386cc704cd847f17d6510f
Sebastián Aguilera <sebastian@procodigo.cl>
2023-09-21 15:18:26 -0300

    Corrección posición elementos landing

--- resources/views/frontend/components/location-payment-pink.blade.php
+++ resources/views/frontend/components/location-payment-pink.blade.php
@@ -1,11 +1,11 @@
-<div class="row pe-5 mt-2 mt-sm-4">
-    <div class="col-md-4 px-0 px-sm-1 text-center mb-4">
+<div class="row pe-4 mt-2 mt-sm-4">
+    <div class="col-md-6 px-0 px-sm-3 text-center mb-4">
         <img src="{{ asset('images/icon_loction_r.svg') }}" class="img-fluid">
     </div>
-    <div class="col-md-4 px-0 px-sm-1 text-center mb-4">
-        <img src="{{ asset('images/icon_loction_r.png') }}" class="img-fluid">
-    </div>
-    <div class="col-md-4 px-0 px-sm-1 text-center mb-4">
+    <div class="col-md-6 px-0 px-sm-3 text-center mb-4">
         <img src="{{ asset('images/Pay_drsmile.png') }}" class="img-fluid">
+    </div>
+    <div class="col-md-6 px-0 px-sm-3 text-center mb-4">
+        <img src="{{ asset('images/icon_loction_r.png') }}" class="img-fluid">
     </div>
 </div>
