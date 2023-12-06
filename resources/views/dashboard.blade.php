8c20fb3aff3b450fd0f97d8ef29dd59de00441ac
Sebastián Aguilera <sebastian@procodigo.cl>
2023-12-06 11:25:30 -0300

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('adminlte::page')
 
-@section('title', 'Dr Smile - Inicio')
+@section('title', 'DrSmile - Inicio')
 
 @section('content_header')
     <h1 class="m-0 text-dark">Dashboard</h1>
