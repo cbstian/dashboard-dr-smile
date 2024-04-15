47c0feac49c0d57c23204f2a2fb7fa87e75ebc1c
Alvaro Guajardo <a.guajardo@canalcero.com>
2024-04-15 15:23:01 -0400

    update message and price ortho

--- resources/views/frontend/ortodoncia.blade.php
+++ resources/views/frontend/ortodoncia.blade.php
@@ -97,8 +97,9 @@
                                     </div>
                                     <div class="row">
                                         <div class="col-md-4 text-start">
-                                            <h1  class="mb-0" style="font-weight: 800; font-size: 35px;"><span style="color:#D9A6E8; font-size:25px;">$</span>249.900</h1>
-                                            <p style="color:#B2B2B2; font-weight: 800; font-size: 19px;">ANTES: $711.898</p>
+                                            <p style="color:#B2B2B2; font-weight: 800; font-size: 19px;">DESDE</p>
+                                            <h1  class="mb-0" style="font-weight: 800; font-size: 35px;"><span style="color:#D9A6E8; font-size:25px;">$</span>1.950.000</h1>
+                                            
                                         </div>
 
                                         <div class="col-md-8 text-start">
