1d7e0b7fc1809ec00f4f9203eb7fd1be3ba92b01
Alvaro Guajardo <a.guajardo@canalcero.com>
2024-07-12 12:40:08 -0400

    cambios solicitados para las landing de botox 2 y 3 zonas

--- resources/views/frontend/components/location-payment-pink.blade.php
+++ resources/views/frontend/components/location-payment-pink.blade.php
@@ -24,7 +24,7 @@
 
 <div class="row pe-4 mt-2 mt-sm-4">
     <div class="col-md-6 px-0 px-sm-3 text-center mb-4">
-        <img src="{{ asset('images/icon_loction_r.svg') }}" class="img-fluid alvaro">
+        <img src="{{ asset('images/icon_loction_r.svg') }}" class="img-fluid">
     </div>
     <div class="col-md-6 px-0 px-sm-3 text-center mb-4">
         <img src="{{ asset('images/Pay_drsmile.png') }}" class="img-fluid">
